376e4412daf3355d9c49fd6f495d58677d15b218
captainboogey <anik35-700@diu.edu.bd>
2025-03-12 14:49:04 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require ('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require("jsonwebtoken")
+const cookieParser = require('cookie-parser');
 const app =express();
 const port = process.env.PORT || 5000;
 
 
 // middlewares
 app.use(express.json());
-app.use(cors());
-require('dotenv').config()
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+  
+
+}));
+// require('dotenv').config()
+app.use(cookieParser())
 
 
 
@@ -23,6 +31,27 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+const cookieOption ={
+  httpOnly:true,
+  secure:false,
+  sameSite:true
+
+}
+// middlewares--------------------
+
+const verifyToken = (req,res,next)=>{
+  const token = req?.cookies?.token;
+  if(!token){
+  return  res.status(401).send({status:"UnAuthorized Access"})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({status:"UnAuthorized Access"})
+    }
+    req.user =decoded
+    next()
+  })
+}
 
 async function run() {
   try {
@@ -30,6 +59,19 @@ async function run() {
 const userCollection = client.db("travelDB") .collection("userDB");
 const dataCollection = client.db("travelDB") .collection("dataDB");
 const bookingCollection = client.db("travelDB").collection("bookDB")
+
+// jwt-------------------------------------------------------------
+
+app.post("/jwt",async(req,res)=>{
+  const user = req.body;
+  const token = jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+  res
+  .cookie('token',token,cookieOption)
+  .send({success:true})
+})
+app.post('/logout',async(req,res)=>{
+  res.clearCookie('token',{...cookieOption,maxAge:0}).send({success:'cookie removed'})
+})
 
 //  usermanagement api---------------------------------------------
 app.post("/users",async(req,res)=>{
@@ -121,8 +163,11 @@ app.post('/book',async(req,res)=>{
 
 // })
 
-app.get('/book',async(req,res)=>{
+app.get('/book',verifyToken,async(req,res)=>{
   // const query = req.query?.email;
+  if(req.query?.email !== req.user.email){
+    return res.status(403).send({status:"forbidden Access"})
+  }
   let query ={};
   if(req.query?.email){
     query = {email: req.query.email}
